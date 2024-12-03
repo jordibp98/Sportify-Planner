1f6c25a455614351bed264f05291feb5c87ff880
jordibp98 <jordibp98@gmail.com>
2024-12-03 10:50:59 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template >
   <div>
-    <h1 class="text-8xl flont-black text-emerald-800">{{ title }}</h1>
+    <h1 class="text-8xl flont-black text-emerald-800"> {{ title }}</h1>
   </div>
 </template>
 
 
 <script>
 export default {
-  name: 'App',
+  name:"App",
   data(){
-    return(
-      title: 'Holaaa!'
-    )
+    return{
+      title: "Holaaa!"
+    }
   }
 }
 </script>

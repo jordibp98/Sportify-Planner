7090f47bede51eff65e59158b327a1fbd316ffa9
jordibp98 <jordibp98@gmail.com>
2024-12-03 10:33:20 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template >
+  <div>
+    <h1 class="text-8xl flont-black text-emerald-800">{{ title }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return(
+      title: 'Holaaa!'
+    )
+  }
+}
+</script>
+
+
+<style>
+  
+</style>
